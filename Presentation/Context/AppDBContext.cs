d6e1b90384563d0242605a81cd7f02572120876f
annecaroli <carolinesilva4@hotmail.com>
2024-05-14 21:48:36 -0300

    Config application, domain and persistence

---
+++ Presentation/Context/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persistence.Context
+{
+    public class AppDBContext: DbContext 
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Presentation/Repositories/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persistence.Repositories;
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDBContext _context;
+
+    public UnitOfWork(AppDBContext context)
+    {
+        _context = context;
+    }
+    public async Task Commit(CancellationToken cancellationToken)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
